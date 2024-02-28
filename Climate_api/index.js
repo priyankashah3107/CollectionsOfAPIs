740d037c55e03f49155f6ca0ec9d320c007de523
Priyanka <priyankashah3107@gmail.com>
2024-02-28 23:21:10 +0530

    SuccessFully fetch Data

--- Climate_api/index.js
+++ Climate_api/index.js
@@ -1,15 +1,42 @@
-const PORT = 7000
+const PORT = 7000;
 
-const express = require("express")
-const axios = require("axios")
-const cheerio = require("cheerio")
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const app = express()
-
+const app = express();
+const stroeArticals = []; 
 app.get("/", (req, res) => {
-  res.json("Hola! Welcome to Climate change API")
-} )
+  res.json("Hola! Welcome to Climate change API");
+});
+
+app.get("/news", (req, res) => {
+ 
+
+  // axios.get("https://science.nasa.gov/climate-change/")
+  axios.get("https://www.theguardian.com/environment/climate-crisis")
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text().trim();
+        const url = $(this).attr('href');
+
+        stroeArticals.push({
+          title,
+          url
+        });
+      });
+
+      res.json(stroeArticals);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
-})
+  console.log(`App is running on http://localhost:${PORT}`);
+});
